99bb0d966612038b96b2b4671f82f09d0fedebd3
abquadir <abquadir73@yah00.com>
2020-09-26 02:06:20 +0530

    website

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll',()=>{
+    var header= document.querySelector('header')
+    header.classList.toggle('sticky',window.scrollY>0)
+})
+
+ScrollOut({
+    targets:'div'
+  });
